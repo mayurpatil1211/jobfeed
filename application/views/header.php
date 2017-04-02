ae8ed56085842d9aa1058d5f74e36e67ff3091e2
mayurpatil1211 <mayur.patil1211@gmail.com>
2017-04-02 18:50:01 +0530

    anglar validation

--- application/views/header.php
+++ application/views/header.php
@@ -4,6 +4,7 @@
       <title>POST JOB</title>
 
 <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url();?>css/style.css"> 
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
